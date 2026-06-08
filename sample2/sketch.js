a37888ed7879c189c06bff484b4f625abd1f9ab5
Lizamong <34539466+Lizamong@users.noreply.github.com>
2026-06-08 17:12:11 +0900

    Update sketch.js

--- sample2/sketch.js
+++ sample2/sketch.js
@@ -1,28 +1,23 @@
 function setup() {
   createCanvas(600, 400);
-  noLoop();
 }
 
 function draw() {
-  background(210, 225, 200); 
+  background(210, 225, 200);
 
-  // 몸통
   noStroke();
   fill(45, 65, 35);
   rect(150, 320, 300, 100, 50, 50, 0, 0);
 
-  // 얼굴형
   stroke(0);
   strokeWeight(2);
   fill(255, 224, 189);
   ellipse(300, 210, 180, 210);
 
-  // 귀
   strokeWeight(1);
   ellipse(210, 220, 12, 25);
   ellipse(390, 220, 12, 25);
 
-  // 군복 디테일
   fill(30, 30, 30);
   noStroke();
   rect(200, 350, 40, 25);
@@ -32,29 +27,45 @@ function draw() {
   line(205, 363, 235, 363);
   line(205, 369, 235, 369);
 
-  // 눈
   noStroke();
   fill(255);
   ellipse(265, 215, 30, 20);
   fill(0);
   circle(265, 215, 12);
   fill(255);
-  circle(267, 213, 4);
+  circle(268, 212, 4);
+
+  stroke(180, 130, 110);
+  line(300, 235, 300, 250);
+
+  fill(35, 45, 30);
+  noStroke();
+  arc(300, 160, 190, 130, 3.14, 6.28);
+  rect(205, 160, 190, 15);
 
   fill(255);
+  noStroke();
   ellipse(335, 215, 30, 20);
   fill(0);
   circle(335, 215, 12);
   fill(255);
-  circle(337, 213, 4);
+  circle(338, 212, 4);
 
-  // 코와 입
-  stroke(180, 130, 110);
-  line(300, 235, 300, 250);
+  noFill();
+  stroke(150, 50, 50);
+  arc(300, 275, 40, 20, 0, 3.1);
 
-  // 베레모 군모
-  fill(35, 45, 30);
+  fill(25, 35, 20);
   noStroke();
-  arc(300, 130, 185, 110, 3.14, 6.28);
-  rect(205, 125, 188, 15, 4);
+  ellipse(300, 175, 210, 35);
+  fill(0);
+  rect(210, 175, 4, 35);
+  rect(386, 175, 4, 35);
+
+  fill(255);
+  textSize(20);
+  textAlign(CENTER);
+  text("ROKA", 300, 150);
+  
+  noLoop();
 }
