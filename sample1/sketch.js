05ae87ebd7647625bd9cb0677a72562e55eb6aef
Lizamong <34539466+Lizamong@users.noreply.github.com>
2026-06-08 17:09:54 +0900

    Update sketch.js

--- sample1/sketch.js
+++ sample1/sketch.js
@@ -1,38 +1,45 @@
 function setup() {
   createCanvas(600, 400);
-  noLoop(); 
+  background("#000000");
+  noLoop();
 }
 
 function draw() {
-  background("#000000"); 
-
-  // 외곽 궤도 라인
-  push();
-  translate(300, 200);
   noFill();
   stroke("#1F2833");
   strokeWeight(2);
-  ellipse(0, 0, 550, 380); 
-  pop();
+  ellipse(300, 200, 550, 380);
 
-  // 중간 성운 레이어
-  push();
-  translate(300, 200);
-  noFill();
   stroke("#66FCF1");
   strokeWeight(4);
-  arc(0, 0, 450, 300, 4.7, 0);  
+  arc(300, 200, 450, 300, 4.7, 0); 
+  
   stroke("#45A29E");
   strokeWeight(6);
-  arc(0, 0, 350, 230, 1.5, 3.1);
-  pop();
+  arc(300, 200, 350, 230, 1.5, 3.1);
 
-  // 중심부 항성 배치
   noStroke();
   fill("#1F2833");
-  ellipse(300, 200, 280, 180); 
+  ellipse(300, 200, 280, 180);
+  
   fill("#45A29E");
   ellipse(300, 200, 200, 130);
+  
   fill("#66FCF1");
   ellipse(300, 200, 120, 80);
+
+  stroke("#FFD700");
+  strokeWeight(1);
+  line(300, 150, 300, 250);
+  line(200, 200, 400, 200);
+
+  noStroke();
+  fill("#FF4500");
+  ellipse(300, 200, 60, 60); 
+
+  fill("#0077BE");
+  ellipse(80, 85, 25, 25); 
+
+  fill("#C0C0C0"); 
+  ellipse(115, 75, 10, 10); 
 }
